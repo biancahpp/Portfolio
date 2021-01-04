1e3e396d768752607fcba754b6a890de1611d3f6
biancahpp <bianca_procopio@hotmail.com>
2021-01-03 18:56:22 -0600

    portfolio page layout done

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,36 +1,50 @@
-import React from 'react'
-import Logo from '../components/Logo';
+import React from "react";
+import Logo from "../components/Logo";
 
-// import Todo from '../images/Todo.png';
-// import IpTracker from '../images/IPtracker.jpg';
+import Todo from "../images/Todo.png";
+import IpTracker from "../images/IPtracker.jpg";
 
 export default function Portfolio() {
   return (
     <div className="Portfolio">
-      <Logo className="logo"/>
-
-      {/* <div className="teste1">
-        <div className="teste">
-          <a 
-          href="https://affectionate-khorana-2a1719.netlify.app/" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="project-link--1">
+      <Logo className="logo" />
+      <span className="Portfolio__title">Portfolio</span>
+      <span className="Portfolio__description">
+        /* These are some projects that I've been working on for the past few
+        months */
+      </span>
+      <div className="projects-container">
+        <div className="project-image">
+          <a
+            href="https://affectionate-khorana-2a1719.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-link--1"
+          >
             <img src={Todo} alt="Todo list"></img>
-            <h3>To-do List</h3>
-          </a>
-          </div>
-          <div className="teste">
-          <a 
-          href="https://eloquent-engelbart-326571.netlify.app/" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="project-link--1">
-            <img src={IpTracker} alt="IP tracker"></img>
-            <h3>Ip tracker</h3>
           </a>
         </div>
-        </div> */}
+        <div className="project-image">
+          <a
+            href="https://eloquent-engelbart-326571.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-link--1"
+          >
+            <img src={IpTracker} alt="IP tracker"></img>
+          </a>
+        </div>
+        <div className="project-image">
+          <a
+            href="https://eloquent-engelbart-326571.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-link--1"
+          >
+            <img src={IpTracker} alt="IP tracker"></img>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
