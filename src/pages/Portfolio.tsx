13f2b568212b6d31d4fd201b513ed8273bb0554a
biancahpp <bianca_procopio@hotmail.com>
2021-01-05 21:26:00 -0600

    working on responsiveness next

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../components/Logo";
-import projects from '../projects/projects.json';
+import projects from "../projects/projects.json";
+import Modal from "react-modal";
+import { FaTimes, FaEye, FaCode } from "react-icons/fa";
 
 export default function Portfolio() {
+  const [selectedModal, setSelectedModal] = useState(-1);
 
   return (
     <div className="Portfolio">
@@ -14,13 +17,52 @@ export default function Portfolio() {
         months */
       </span>
       <div className="projects-container">
-        
-        {projects.map(item => 
-        <div className="project-image">
-          <img src={item.image} alt="project preview"></img>
-        </div>
-        )}
-        
+        {projects.map((item, index) => (
+          <><div className="project-image" onClick={() => setSelectedModal(index)}>
+            <img src={item.image} alt="project preview"></img>
+          </div>
+            <Modal
+              isOpen={index === selectedModal}
+              onRequestClose={() => setSelectedModal(-1)}
+              className="project-modal"
+              overlayClassName="project-modal-overlay"
+            >
+              <FaTimes
+                onClick={() => setSelectedModal(-1)}
+                className="close-icon" />
+              <div className="modal-image">
+                <img src={item.image} alt={item.title}></img>
+              </div>
+              <div className="modal-content">
+                <h1>{item.title}</h1>
+                <div className="modal-technologies">
+                  {item.tags.map(it => <span>{it}</span>)}
+                </div>
+                <h3>About</h3>
+                <p>{item.description}</p>
+                <div className="btn-link-wrapper">
+                  <a
+                    href={item.demoUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="project-link"
+                  >
+                    <FaEye className="eye-icon" />
+                    <span>Demo</span>
+                  </a>
+                  <a
+                    href={item.codeUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="project-link"
+                  >
+                    <FaCode className="code-icon" />
+                    <span>Code</span>
+                  </a>
+                </div>
+              </div>
+            </Modal></>
+        ))}
       </div>
     </div>
   );
