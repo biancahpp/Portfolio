e6af82f39e1f93a03701f8e76f850c496ea1cf74
biancahpp <bianca_procopio@hotmail.com>
2021-02-03 18:03:35 -0600

    updating resume

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -36,7 +36,7 @@ export default function Header() {
         </div>
         <div className="btn-wrapper">
         <a 
-          href="https://drive.google.com/file/d/1qxpSyQnsMz4z3mmWnvSrHCqeBKyuBU1y/view?usp=sharing" 
+          href="https://drive.google.com/file/d/1sAK49_fPv4uMFPaJ6nz5qH9GUaFWoSzB/view?usp=sharing" 
           target="_blank" 
           rel="noopener noreferrer" 
           className="btn-resume">
